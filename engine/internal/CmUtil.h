91200f6bff649db5c9775bc4114b65e3c41a57bc
kimminkyeu <minkyeki@student.42seoul.kr>
2024-01-14 20:02:34 +0900

    Feat : Shared library export macro generation by cmake

--- engine/internal/CmUtil.h
+++ engine/internal/CmUtil.h
@@ -1,7 +1,11 @@
 #ifndef ENGINE_INTERNAL_CMUTIL_H
 #define ENGINE_INTERNAL_CMUTIL_H
 
-// HEADER ONLY
+// cmake generated header for CHIMERA_API macro
+#include "chimera_api_export.h"
+
+// chimera util headers
+#include "cm_util/PlatformDetection.h"
 #include "cm_util/Assert.h"
 #include "cm_util/Sal.h"
 #include "cm_util/NonCopyable.h"
