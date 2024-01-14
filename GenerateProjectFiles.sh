063b11e053ca521b95ceb5c7f884e528a6fb780c
kimminkyeu <minkyeki@student.42seoul.kr>
2024-01-14 20:21:35 +0900

    Fix : 최종 빌드 테스트 후 인클루드 경로 문제 해결

--- GenerateProjectFiles.sh
+++ GenerateProjectFiles.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 rm -rf "$PWD/engine/build/intermediate"
-cmake -S "$PWD/engine" -B "$PWD/engine/build/intermediate" -DBUILD_SHARED_LIBS=ON -DCMAKE_VERBOSE_MAKEFILE=TRUE -DCMAKE_BUILD_TYPE=Debug
+cmake -S "$PWD/engine" -B "$PWD/engine/build/intermediate" -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Debug
 
 # build
 cmake --build "$PWD/engine/build/intermediate" -j4
